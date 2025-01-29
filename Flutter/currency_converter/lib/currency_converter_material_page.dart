afb480105d383cd834de8fc53e73819ec7f6f156
HevertonLessa <hevertonnegocios21@gmail.com>
2025-01-29 08:32:39 +0000

    29/01/25

--- Flutter/currency_converter/lib/currency_converter_material_page.dart
+++ Flutter/currency_converter/lib/currency_converter_material_page.dart
@@ -5,9 +5,32 @@ class MaterialHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
+      backgroundColor: Color.fromRGBO(33, 33, 33, 1),
       body: Center(
-        child: Text('Hello World!'),
+        child: Column (
+          mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('0', style: TextStyle(
+            fontSize: 35,
+            fontWeight: FontWeight.bold,
+            color: Color.fromARGB(255, 255, 255, 255),
+          ),
+          ),
+          TextField(
+            style: TextStyle(
+              color: Color.fromARGB(255, 255, 253, 253)
+            ),
+            decoration: InputDecoration(
+              label: Text('Please enter the amount', 
+              style: TextStyle(
+                  color: Color.fromARGB(131, 255, 255, 255)
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
+      )
     );
   }
 }
