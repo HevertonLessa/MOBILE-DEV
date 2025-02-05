eaf19e7ac0f6e4f94adc8b98aef71e38a5b10d64
HevertonLessa <hevertonnegocios21@gmail.com>
2025-02-05 00:00:17 +0000

    05/02/25

--- Flutter/currency_converter/lib/currency_converter_material_page.dart
+++ Flutter/currency_converter/lib/currency_converter_material_page.dart
@@ -30,20 +30,46 @@ class MaterialHomePage extends StatelessWidget {
                 color: Color.fromARGB(255, 255, 255, 255),
               ),
             ),
-            TextField(
-              style: TextStyle(color: Color.fromARGB(255, 255, 253, 253)),
-              decoration: InputDecoration(
-                hintText: 'Please enter the amount',
-                hintStyle: TextStyle(
-                  color: Color.fromARGB(160, 255, 255, 255),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+              child: TextField(
+                style: TextStyle(color: Color.fromARGB(255, 255, 253, 253)),
+                decoration: InputDecoration(
+                  hintText: 'Please enter the amount',
+                  hintStyle: TextStyle(
+                    color: Color.fromARGB(160, 255, 255, 255),
+                  ),
+                  prefixIcon: Icon(Icons.monetization_on_outlined),
+                  prefixIconColor: Colors.white,
+                  filled: true,
+                  fillColor: Color.fromARGB(57, 155, 155, 155),
+                  focusedBorder: border,
+                  enabledBorder: border,
                 ),
-                prefixIcon: Icon(Icons.monetization_on_outlined),
-                prefixIconColor: Colors.white,
-                filled: true,
-                fillColor: Color.fromARGB(57, 155, 155, 155),
-                focusedBorder: border,
-                enabledBorder: border,
+                keyboardType: TextInputType.numberWithOptions(
+                  decimal: true,
+                ),
               ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(0),
+              child: ElevatedButton(
+                  onPressed: () {
+                    debugPrint('button clicked');
+                  },
+                  style: ElevatedButton.styleFrom(
+                    surfaceTintColor: (Colors.black),
+                    overlayColor: (const Color.fromARGB(107, 0, 0, 0)),
+                    backgroundColor:(Color.fromARGB(57, 155, 155, 155)),
+                    foregroundColor: (Colors.white),
+                    fixedSize: (Size(double.infinity, 30)),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(70),
+                    ), 
+                  ),
+                  child: const Text('Convert'),
+                  
+                      ),
             ),
           ],
         ),
