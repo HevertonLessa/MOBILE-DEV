e5c513a985f80433667fd62e9b08ab92e146c9dd
HevertonLessa <hevertonnegocios21@gmail.com>
2025-02-10 23:38:36 +0000

    10/02/25

--- Flutter/currency_converter/lib/currency_converter_material_page.dart
+++ Flutter/currency_converter/lib/currency_converter_material_page.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
 
-class MaterialHomePage extends StatelessWidget {
+class MaterialHomePage extends StatefulWidget {
   const MaterialHomePage({super.key});
 
   @override
+  State createState() => _MaterialHomePage();
+}
+
+class _MaterialHomePage extends State {
+  double result = 0;
+  @override
   Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
+
+class MaterialHomePagee extends StatelessWidget {
+  const MaterialHomePagee({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    TextEditingController textEditingController = TextEditingController();
+
     final border = OutlineInputBorder(
         borderSide: BorderSide(
           color: const Color.fromARGB(170, 0, 0, 0),
@@ -18,12 +35,29 @@ class MaterialHomePage extends StatelessWidget {
 
     return Scaffold(
       backgroundColor: Color.fromRGBO(58, 56, 56, 1),
+      appBar: AppBar(
+        backgroundColor: Color.fromRGBO(
+          58,
+          56,
+          56,
+          1,
+        ),
+        elevation: 0,
+        centerTitle: true,
+        title: Text(
+          'Currency Converter',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        actions: [],
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              '0',
+              result.toString(),
               style: TextStyle(
                 fontSize: 35,
                 fontWeight: FontWeight.bold,
@@ -33,6 +67,7 @@ class MaterialHomePage extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
               child: TextField(
+                controller: textEditingController,
                 style: TextStyle(color: Color.fromARGB(255, 255, 253, 253)),
                 decoration: InputDecoration(
                   hintText: 'Please enter the amount',
@@ -54,22 +89,21 @@ class MaterialHomePage extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.all(0),
               child: ElevatedButton(
-                  onPressed: () {
-                    debugPrint('button clicked');
-                  },
-                  style: ElevatedButton.styleFrom(
-                    surfaceTintColor: (Colors.black),
-                    overlayColor: (const Color.fromARGB(107, 0, 0, 0)),
-                    backgroundColor:(Color.fromARGB(57, 155, 155, 155)),
-                    foregroundColor: (Colors.white),
-                    fixedSize: (Size(double.infinity, 30)),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(70),
-                    ), 
+                onPressed: () {
+                  result = double.parse(textEditingController.text) * 81;
+                },
+                style: ElevatedButton.styleFrom(
+                  surfaceTintColor: (Colors.black),
+                  overlayColor: (const Color.fromARGB(107, 0, 0, 0)),
+                  backgroundColor: (Color.fromARGB(57, 155, 155, 155)),
+                  foregroundColor: (Colors.white),
+                  fixedSize: (Size(double.infinity, 30)),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(70),
                   ),
-                  child: const Text('Convert'),
-                  
-                      ),
+                ),
+                child: const Text('Convert'),
+              ),
             ),
           ],
         ),
