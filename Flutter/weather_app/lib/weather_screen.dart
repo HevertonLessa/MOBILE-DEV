f081a097dd9d3a673543680708e0a0cc34f9251d
HevertonLessa <hevertonnegocios21@gmail.com>
2025-02-26 09:39:30 +0000

    25/02/25

--- Flutter/weather_app/lib/weather_screen.dart
+++ Flutter/weather_app/lib/weather_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class WeatherScreen extends StatelessWidget {
@@ -14,28 +16,69 @@ class WeatherScreen extends StatelessWidget {
         ),
         actions: [
           IconButton(
-            onPressed: () {}, 
+            onPressed: () {},
             icon: Icon(Icons.refresh),
           )
         ],
       ),
-      body: Column(
-        children: [
-          //main card
-          const Placeholder(
-            fallbackHeight: 250,
-          ),
-          // weather forecast cards
-          const SizedBox(height: 25),
-          const Placeholder(
-            fallbackHeight: 150,
-          ),
-          // additional information
-          const SizedBox(height: 25),
-          const Placeholder(
-            fallbackHeight: 150,
-          ),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            //main card
+            SizedBox(
+              width: double.infinity,
+              child: Card(
+                color: const Color.fromARGB(255, 41, 45, 47),
+                elevation: 10,
+                shadowColor: Color.fromARGB(255, 74, 74, 74),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16)),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(16),
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16),
+                      child: Column(
+                        children: [
+                          Text(
+                            '300 °F',
+                            style: TextStyle(
+                              fontSize: 32,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 16,
+                          ),
+                          Icon(
+                            Icons.cloud,
+                            size: 125,
+                          ),
+                          Text(
+                            'Rain',
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            // weather forecast cards
+            const SizedBox(height: 25),
+            const Placeholder(
+              fallbackHeight: 150,
+            ),
+            // additional information
+            const SizedBox(height: 25),
+            const Placeholder(
+              fallbackHeight: 150,
+            ),
+          ],
+        ),
       ),
     );
   }
